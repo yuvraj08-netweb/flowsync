98dafefd39a5a5815eb5ce0ac9cd7a3814023cdb
Yuvraj <yuvraj.netweb@gmail.com>
2025-01-31 17:32:25 +0530

    changed the footer text

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -92,7 +92,7 @@ const Footer1 = () => {
           <div className="flex gap-8 flex-col items-start">
             <div className="flex gap-2 flex-col">
               <h2 className="text-3xl md:text-5xl tracking-tighter max-w-xl font-regular text-left">
-                TWBlocks™
+                FlowSync™
               </h2>
               <p className="text-lg max-w-lg leading-relaxed tracking-tight text-background/75 text-left">
                 Managing a small business today is already tough.
